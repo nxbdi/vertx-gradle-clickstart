8082aea2fa4d177a579366487e56f1d6fadce8ca
Cyrille Le Clerc <cyrille@cyrilleleclerc.com>
2013-09-18 01:32:31 +0100

    Add Database dependency

--- src/main/java/localdomain/localhost/DemoVerticle.java
+++ src/main/java/localdomain/localhost/DemoVerticle.java
@@ -15,9 +15,14 @@
  */
 package localdomain.localhost;
 
+import com.bloidonia.vertx.mods.JsonUtils;
+import org.vertx.java.core.AsyncResult;
 import org.vertx.java.core.Handler;
+import org.vertx.java.core.eventbus.EventBus;
+import org.vertx.java.core.eventbus.Message;
 import org.vertx.java.core.http.HttpServer;
 import org.vertx.java.core.http.HttpServerRequest;
+import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.platform.Verticle;
 
 /**
@@ -31,10 +36,13 @@ public class DemoVerticle extends Verticle {
 
         container.logger().info("Start " + getClass().getName() + " on " + port);
 
+        final EventBus eventBus = vertx.eventBus();
+
         final HttpServer server = vertx.createHttpServer();
         server.requestHandler(new Handler<HttpServerRequest>() {
             @Override
             public void handle(HttpServerRequest event) {
+
                 event.response().putHeader("Content-Type", "text/html");
                 event.response().end("<html>" +
                         "<head><title>Vertx ClickStart</title></head>" +
@@ -44,6 +52,51 @@ public class DemoVerticle extends Verticle {
                         "</html>");
             }
         }).listen(port);
+
+        final JsonObject persistorConfig;// = container.config().getObject("db_mydb");
+        persistorConfig = new JsonObject() {{
+            putString("address", "com.bloidonia.jdbcpersistor");
+            putString("driver", "com.mysql.jdbc.Driver");
+            putString("url", "jdbc:" + System.getProperty("DATABASE_URL_MYDB"));
+            putString("username", System.getProperty("DATABASE_USERNAME_MYDB"));
+            putString("password", System.getProperty("DATABASE_PASSWORD_MYDB"));
+        }};
+
+
+        final String persistorAddress = persistorConfig.getString("address");
+
+        container.logger().info("Connect to DB " + persistorConfig.getString("url") + " - " + persistorConfig.getString("username"));
+
+        container.logger().info("deploy module com.bloidonia~mod-jdbc-persistor~2.1 as a WorkerVerticle");
+        container.deployWorkerVerticle("com.bloidonia.vertx.mods.JdbcProcessor", persistorConfig, 1, true, new Handler<AsyncResult<String>>() {
+            @Override
+            public void handle(AsyncResult<String> event) {
+
+                if (event.failed()) {
+                    container.logger().warn("Failure to initialize JDBC Processor");
+                } else {
+                    String result = event.result();
+                    container.logger().info("JDBC Processor initialized: " + result);
+
+                    JsonObject select1Msg = new
+
+                            JsonObject() {{
+                                putString("action", "select");
+                                putString("stmt", "select 1");
+                            }};
+
+
+                    container.logger().info("send select 1");
+                    eventBus.send(persistorAddress, select1Msg, new Handler<Message<JsonObject>>() {
+                        @Override
+                        public void handle(Message<JsonObject> event) {
+                            container.logger().info("result of select 1: " + event.body().toString());
+
+                        }
+                    });
+                }
+            }
+        });
 
     }
 }
